4c5573aad9d2b2d50363f04bdf5cc0820051a67b
Gustavo Rafael Nunes Duraes <gustavo.ipatinga@gmail.com>
2023-04-11 20:57:37 -0300

    Primeiro Commit

---
+++ src/main/java/br/edu/ifnmg/aluno/grnd/grupostrabalho/Grupo.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.aluno.grnd.grupostrabalho;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author gusta
+ */
+@Entity
+public class Grupo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 65)
+    private String nome;
+
+    private Boolean ativo = Boolean.TRUE;
+
+    //<editor-fold defaultstate="collapsed" desc="Get/Set">
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Hash/Equals/toString">
+    
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        return hashCode() == obj.hashCode();
+    }
+    
+    //</editor-fold>
+
+    
+    
+    @Override
+    public String toString() {
+        return "br.edu.ifnmg.aluno.grnd.grupostrabalho.Grupo[ id=" + id + " ]";
+    }
+
+}
---
+++ src/main/java/br/edu/ifnmg/aluno/grnd/grupostrabalho/Telefone.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.aluno.grnd.grupostrabalho;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author gusta
+ */
+@Entity
+public class Telefone implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private Byte ddd;
+    private Integer numero;
+
+    //<editor-fold defaultstate="collapsed" desc="Get/Set">
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Byte getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(Byte ddd) {
+        this.ddd = ddd;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Hash/Equal/toString">
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        return hashCode() == obj.hashCode();
+    }
+    //</editor-fold>
+    
+
+    @Override
+    public String toString() {
+        return "br.edu.ifnmg.aluno.grnd.grupostrabalho.Telefone[ id=" + id + " ]";
+    }
+    
+}
